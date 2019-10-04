5c6da4dbaf91131bab9f0b1c584eac9950653dd0
Muriel <matheus.muriel@outlook.com>
2019-10-04 14:24:15 -0300

    PyIniquer

---
+++ ExemplePyIniquer.py
@@ -0,0 +1,12 @@
+from __future__ import print_function, unicode_literals
+from PyInquirer import prompt
+from pprint import pprint
+questions = [
+    {
+        'type': 'input',
+        'name': 'first_name',
+        'message': 'Whats your first name',
+     }
+]
+answers = prompt(questions)
+pprint(answers)
